562fe161c1161387d63770166bba53d78a9eb234
Blake Ayers <bayers@stedwards.edu>
2021-03-14 22:32:04 +0000

    Merged in mvp (pull request #1)
    Mvp
    
    * Version 1 for MVP demo
    
    * Fixed path reference
    
    Approved-by: Erik Bautista Santibanez

--- server.py
+++ server.py
@@ -1,16 +1,45 @@
+import json
 from http.server import HTTPServer
 from http.server import BaseHTTPRequestHandler
+from pymongo import MongoClient
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    # Reads the POST data from the HTTP header
+    def extract_POST_Body(self):
+        postBodyLength = int(self.headers['content-length'])
+        postBodyString = self.rfile.read(postBodyLength)
+        postBodyDict = json.loads(postBodyString)
+        return postBodyDict
+
+    # handle post requests
     def do_POST(self):
-        return
+        # status = 200
+        # self.send_response(status)
+        # self.send_header("Content-type", "text/html")
+        # self.end_headers()
+
+        # store POST data into a dictionary
+        postData = self.extract_POST_Body()
+        path = self.path
+
+        # receiving registration requests and writing their data to the demand database
+        if '/register' in path:
+            client = MongoClient('demand.team22.sweispring21.tk:27017', username="developer", password="team22_developer")
+            db = client.team22_demand
+            db.user.insert_one({"username": postData["username"],
+                                "fname": postData["fname"],
+                                "lname": postData["lname"],
+                                "address": postData["address"],
+                                "password": postData["password"]})
+            client.close()
+
+
+
 
     def do_GET(self):
         return
 
 def main():
-    # This is a demo port. Once we start adding things, like creating users, ect..., we can define different
-    # servers based on port number
     port = 4001
     server = HTTPServer(('', port), SimpleHTTPRequestHandler)
     print('Server is starting... Use <Ctrl+C> to cancel. Running on Port 8080')
