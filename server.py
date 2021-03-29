99de5b4ce542fdb5901c9673a056ebe9e5fbb3a3
blake <bayers@stedwards.edu>
2021-03-28 22:29:08 -0500

    Verifying that requestVehicle is returning correct value

--- server.py
+++ server.py
@@ -34,18 +34,19 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
             ##### TEMPORARY just sending the order number back from the supply side as a response
             temporaryResponse = order.requestVehicle()
-            status = 200
-            responseBody = {
-                'status': 'success',
-                'message': str(temporaryResponse)
-            }
+            if (temporaryResponse == 0000):
+                status = 200
+                responseBody = {
+                    'status': 'success',
+                    'message': str(temporaryResponse)
+                }
 
-            self.send_response(status)
-            self.send_header("Content-Type", "text/html")
-            self.end_headers()
-            responseString = json.dumps(responseBody).encode('utf-8')
-            self.wfile.write(responseString)
-            client.close()
+        self.send_response(status)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        responseString = json.dumps(responseBody).encode('utf-8')
+        self.wfile.write(responseString)
+        client.close()
 
     def do_GET(self):
         return
